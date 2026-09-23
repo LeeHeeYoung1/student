76783b781ab729bdbbaea32e3dbaef3d09d9c7dc
웅 <eacella1@gmail.com>
2026-09-23 17:20:00 +0900

    controller updated

--- src/main/java/com/kedu/controllers/StudentController.java
+++ src/main/java/com/kedu/controllers/StudentController.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kedu.dao.StudentDAO;
 import com.kedu.dto.StudentDTO;
 
 @Controller
@@ -25,26 +27,28 @@ public class StudentController {
 	
 	
 	@RequestMapping("/list")
-	public String list() {
-		List<StudentDTO> list = dao.;
+	public String list(Model model) {
+		List<StudentDTO> list = dao.studentsList();
+		model.addAttribute("list", list);
 		return "/";
 	}
 	
 	@RequestMapping("/")
-	public String update() {
-		dao.;
+	public String update(StudentDTO dto) {
+		dao.updateList(dto);
 		return "/";
 	}
 	
 	@RequestMapping("/")
-	public String delete() {
-		dao.;
+	public String delete(int id) {
+		dao.deleteList(id);
 		return "/";
 	}
 	
 	@RequestMapping("/")
-	public String search() {
-		List<StudentDTO> list = dao.;
+	public String search(String name, Model model) {
+		List<StudentDTO> list = dao.searchList(name);
+		model.addAttribute("list", list);
 		return "/";
 	}
 }
